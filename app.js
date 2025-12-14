4c0f6b66afba4dc4d3f76e65b45481108fa9175f
anhdh-gh <anhdh.cv@gmail.com>
2025-12-14 18:58:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     // QUAN TRỌNG: Phải có dòng này thì cPanel mới không bị lỗi NoneType
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Server Node.js dang chay!');
+    res.end('Server Node.js is running!');
 });
 
 // CloudLinux tự động quản lý port, không cần điền số
