1e7d3dbf5612091f3960ea782155855de35b47f8
anhdh-gh <anhdh.cv@gmail.com>
2025-12-14 18:32:59 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,39 +3,28 @@ const fs = require('fs');
 const path = require('path');
 
 const server = http.createServer((req, res) => {
-    // 1. Xác định đường dẫn file tuyệt đối
-    // __dirname sẽ lấy chính xác thư mục chứa file app.js trên server
+    // Lấy đường dẫn tuyệt đối của file index.html
     const filePath = path.join(__dirname, 'index.html');
 
-    // 2. Thử đọc file
     fs.readFile(filePath, (err, data) => {
         if (err) {
-            // NẾU CÓ LỖI:
-            console.error('Lỗi khi đọc file:', err); // Ghi vào log hệ thống
-
+            // NẾU CÓ LỖI: In chi tiết ra màn hình thay vì câu chung chung
             res.writeHead(500, {'Content-Type': 'text/plain; charset=utf-8'});
-
-            // QUAN TRỌNG: Dòng này sẽ in lỗi chi tiết ra màn hình trình duyệt của bạn
-            // Ví dụ: "ENOENT" (không tìm thấy file), "EACCES" (không có quyền đọc)...
-            return res.end('GẶP LỖI RỒI: ' + err.message + '\nĐường dẫn file server đang tìm: ' + filePath);
+            res.end(
+                '--- THÔNG BÁO LỖI CHI TIẾT ---\n' +
+                '1. Mã lỗi (Code): ' + err.code + '\n' +
+                '2. Lý do (Message): ' + err.message + '\n' +
+                '3. Đường dẫn server đang tìm: ' + filePath + '\n' +
+                '4. Thư mục hiện tại (__dirname): ' + __dirname
+            );
+            return;
         }
 
-        // NẾU THÀNH CÔNG:
+        // NẾU THÀNH CÔNG
         res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
         res.end(data);
     });
 });
 
 const io = require('socket.io')(server);
-
-io.on('connection', (socket) => {
-    console.log('Client connected: ' + socket.id);
-    socket.emit('server_message', 'Chào mừng đến với hang Ma Sói!');
-    socket.on('chat_message', (msg) => {
-        io.emit('server_message', 'Người lạ: ' + msg);
-    });
-});
-
-server.listen(() => {
-    console.log('Server is running');
-});
+server.listen(); // Để trống để cPanel tự điền port
