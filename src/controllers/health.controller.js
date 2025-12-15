a56004590d2c0158a0f12d54493aab10fb29a828
anhdh-gh <anhdh.cv@gmail.com>
2025-12-15 22:19:04 +0700

    Update health.controller.js

--- src/controllers/health.controller.js
+++ src/controllers/health.controller.js
@@ -5,20 +5,13 @@ const db = require('../config/database'); // ← pool mysql2
 
 exports.health = async (req, res, next) => {
     try {
-        // simple ping DB
-        await db.execute('SELECT 1');
-
         return success(res, {
             status: 'UP',
         });
 
     } catch (err) {
         return next(
-            new AppError(
-                ERROR_CODES.INTERNAL_ERROR,
-                500,
-                'Database connection failed'
-            )
+            new AppError(ERROR_CODES.INTERNAL_ERROR)
         );
     }
 };
