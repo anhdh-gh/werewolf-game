bf25be37fe64f7362f483006cc646333ba6c5e70
anhdh-gh <anhdh.cv@gmail.com>
2025-12-15 20:21:19 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(logger);
 // ===== ROUTES =====
 app.use('/api', routes);
 
-// ===== ERROR HANDLER (Bottom) =====
+// ===== ERROR HANDLER =====
 app.use(errorHandler);
 
 module.exports = app;
