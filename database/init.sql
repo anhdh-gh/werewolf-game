db81222882d8fcf85dac3511933ed881095e620d
anhdh-gh <anhdh.cv@gmail.com>
2025-12-23 01:00:13 +0700

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -24,7 +24,7 @@ CREATE TABLE rooms
         'DAY_VOTE',
         'END'
         )                                               NOT NULL,
-    max_players INT NOT NULL
+    max_players INT NOT NULL,
     phase_expires_at DATETIME,
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     ended_at   DATETIME NULL
