628d6d0486c2d35f1709c874db290d95ce2be334
anhdh-gh <anhdh.cv@gmail.com>
2025-12-31 22:47:56 +0700

    fix(app): change servers

--- src/constants/servers.constant.js
+++ src/constants/servers.constant.js
@@ -1,15 +1,17 @@
 const SERVERS = [
     {
         id: 'EARTH',
+        index: 1,
         name: "Trái Đất",
-        api: 'https://werewolf.anhdh.net',
-        ws: 'wss://werewolf.anhdh.net'
+        api: 'https://werewolf-s1.anhdh.net',
+        ws: 'wss://werewolf-s1.anhdh.net'
     },
     {
         id: 'MARS',
+        index: 2,
         name: "Sao hỏa",
-        api: 'https://werewolf1.anhdh.net',
-        ws: 'wss://werewolf1.anhdh.net'
+        api: 'https://werewolf-s2.anhdh.net',
+        ws: 'wss://werewolf-s2.anhdh.net'
     }
 ];
 
