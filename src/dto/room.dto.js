e449064719084985033aa63ae88565458357056a
anhdh-gh <anhdh.cv@gmail.com>
2026-01-03 18:45:01 +0700

    Update room.dto.js

--- src/dto/room.dto.js
+++ src/dto/room.dto.js
@@ -13,8 +13,8 @@ exports.joinRoomDto = z.object({
         code: z
             .string()
             .trim()
-            .min(5, { message: 'Username must be at least 5 characters' })
-            .max(10, { message: 'Username must be at most 10 characters' }),
+            .min(5, { message: 'Room code must be at least 5 characters' })
+            .max(10, { message: 'Room code must be at most 10 characters' }),
     })
 });
 
