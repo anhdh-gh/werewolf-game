55fab6a3aaedc76641b7c60d627bc8999a714730
nguyenthedat <dnguyenthe706@gmail.com>
2026-01-07 22:15:47 +0700

    first commit

--- src/app/components/Loading.js
+++ src/app/components/Loading.js
@@ -6,7 +6,6 @@ export default function Loading({ textMsg }) {
       <div className="flex flex-col items-center gap-2">
         {/* Spinner */}
         <div className="h-10 w-10 animate-spin rounded-full border-4 border-gray-200 border-t-blue-500" />
-
         {/* Message */}
         <p className="text-gray-500">
           {textMsg || "Werewolf..."}
